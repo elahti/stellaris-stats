d51853cfbd5a8ac2b88875542258a9a4827b7ff8
Esko Lahti <ext-esko.lahti@yle.fi>
2025-11-30 11:27:11 +0200

    Merge to remove npm workspaces commit

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -2,16 +2,12 @@
 
 HOME=/home/vscode
 WORKSPACE=/workspace
-WORKSPACES=$WORKSPACE/workspaces
 
 for DIR in \
   $HOME/.cache \
   $HOME/.claude \
   $HOME/.local \
   $WORKSPACE/gamestate-json-data \
-  $WORKSPACE/node_modules \
-  $WORKSPACES/graphql-server/node_modules \
-  $WORKSPACES/scripts/node_modules \
-  $WORKSPACES/shared/node_modules; do
+  $WORKSPACE/node_modules; do
   sudo chown -R vscode:vscode $DIR
 done
