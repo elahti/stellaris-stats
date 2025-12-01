68ffb1811ea658ab9c6d86cabfb1eac1451c409e
Esko Lahti <ext-esko.lahti@yle.fi>
2025-12-01 15:25:51 +0000

    DB dump & restore

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -7,6 +7,7 @@ for DIR in \
   $HOME/.cache \
   $HOME/.claude \
   $HOME/.local \
+  $WORKSPACE/db-dump-data \
   $WORKSPACE/gamestate-json-data \
   $WORKSPACE/node_modules; do
   sudo chown -R vscode:vscode $DIR
