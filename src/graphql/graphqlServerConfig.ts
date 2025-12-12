b97b19de0f88d21b91c9a4b6a45ee5b3bc985ff8
Esko Lahti <esko.p.lahti@gmail.com>
2025-12-12 07:05:00 +0000

    Remove default values from Redis configuration

--- src/graphql/graphqlServerConfig.ts
+++ src/graphql/graphqlServerConfig.ts
@@ -2,7 +2,7 @@ import z from 'zod/v4'
 
 export const GraphQLServerConfig = z.object({
   STELLARIS_STATS_GRAPHQL_SERVER_PORT: z.coerce.number(),
-  STELLARIS_STATS_REDIS_HOST: z.string().default('redis'),
-  STELLARIS_STATS_REDIS_PORT: z.coerce.number().default(6379),
-  STELLARIS_STATS_REDIS_DB: z.coerce.number().default(0),
+  STELLARIS_STATS_REDIS_HOST: z.string(),
+  STELLARIS_STATS_REDIS_PORT: z.coerce.number(),
+  STELLARIS_STATS_REDIS_DB: z.coerce.number(),
 })
