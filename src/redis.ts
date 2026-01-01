3880334514cc67ed5972d21d4780a1428732a5e4
Esko Lahti <esko.p.lahti@gmail.com>
2026-01-01 13:34:04 +0000

    Make RedisConfig internal to redis module

--- src/redis.ts
+++ src/redis.ts
@@ -1,7 +1,7 @@
 import { Redis } from 'ioredis'
 import z from 'zod/v4'
 
-export const RedisConfig = z.object({
+const RedisConfig = z.object({
   STELLARIS_STATS_REDIS_HOST: z.string().default('redis'),
   STELLARIS_STATS_REDIS_PORT: z.coerce.number().default(6379),
   STELLARIS_STATS_REDIS_DB: z.coerce.number().default(0),
